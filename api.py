514b451c237887e39c97874b084258c2f1fcc7aa
eacosta <emacosta@theworkshop.com>
2020-04-24 00:18:14 +0200

    declarated global var

--- api.py
+++ api.py
@@ -15,6 +15,7 @@ CHAT_ID = 274429781
 DELAY = 60
 NUM_OF_TIMES = 10
 URL = 'https://www.amazon.es/dp/B07X8CVLRP'
+price_under = 30.0
 
 options = Options()
 options.binary_location = GOOGLE_CHROME_BIN
@@ -52,8 +53,8 @@ def scrape():
     #chat_id = data['message']['chat']['id']
     #message = data['message']['text']
 
+    global price_under
     i = 0
-    price_under = 30.0
     while i < NUM_OF_TIMES:
         element = scrape_amazon_price(URL)
 
@@ -72,10 +73,10 @@ def scrape():
                     "text": 'El precio de ' + URL + ' ha bajado de ' + str(price_under) + ' € a ' + str(element) + ' €.',
                 }
 
+                price_under = element
+
                 message_url = BOT_URL + 'sendMessage'
                 requests.post(message_url, json=json_data)
-
-                price_under = element
 
         time.sleep(DELAY)
         i += 1
