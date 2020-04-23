f2c391264ac2e89e4ec025a9799e14d722d15a16
eacosta <emacosta@theworkshop.com>
2020-04-23 19:44:02 +0200

    added new rest

--- api.py
+++ api.py
@@ -1,19 +1,15 @@
 import requests
 import os
+import json
 from flask import Flask, Response, request
 from flask_cors import CORS
-import json
-
-
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, WebDriverException
 from selenium.webdriver.chrome.options import Options
-import os
 
 CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
 GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
 BOT_URL = f'https://api.telegram.org/bot{os.environ["BOT_KEY"]}/'
-
 
 options = Options()
 options.binary_location = GOOGLE_CHROME_BIN
@@ -29,10 +25,6 @@ app = Flask(__name__)
 CORS(app)
 
 
-# url = 'https://www.amazon.es/dp/B006CZ0LGA'
-# driver.get(url)
-
-
 def scrape_amazon_price(url):
     driver.get(url)
 
@@ -43,26 +35,18 @@ def scrape_amazon_price(url):
     return element
 
 
-#@app.route("/")
-#def index():
-#    return 'Scraper alive!'
+@app.route("/")
+def index():
+    return 'Scraper alive!'
 
-
-#@app.route("/api/scrape", methods=['POST'])
 @app.route('/', methods=['POST'])
 def scrape():
-    #if request.json is None:
-    #    error = {"Error": "Missing url"}
-    #    return Response(json.dumps(error), status=400, mimetype='application/json')
-
     data = request.json
 
-    print(data)  # Comment to hide what Telegram is sending you
+    print(data)
     chat_id = data['message']['chat']['id']
     message = data['message']['text']
 
-    #url = request.json.get('url', None)
-    #print(url)
     element = scrape_amazon_price(message)
 
     status = 200 if element is not None else 412
@@ -75,10 +59,23 @@ def scrape():
     message_url = BOT_URL + 'sendMessage'
     requests.post(message_url, json=json_data)
 
-    #response = json.dumps({'Price': element})
-
-    #return Response(response=response, status=status, mimetype='application/json')
     return ''
+
+@app.route("/api/scrape", methods=['POST'])
+def scrape():
+    if request.json is None:
+        error = {"Error": "Missing url"}
+        return Response(json.dumps(error), status=400, mimetype='application/json')
+
+    url = request.json.get('url', None)
+    print(url)
+    element = scrape_amazon_price(message)
+
+    status = 200 if element is not None else 412
+
+    response = json.dumps({'Price': element})
+
+    return Response(response=response, status=status, mimetype='application/json')
 
 
 if __name__ == '__main__':
